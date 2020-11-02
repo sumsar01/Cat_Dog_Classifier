fdbba7dc6a4d747bd2b2ec47e89769c4bf3662f1
sumsar01 <skriver@live.dk>
2020-11-02 14:37:46 +0100

    uploading data and basic libaries

---
+++ Cat_dog_main.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import numpy as np
+import tensorflow as tf
+
+
+
+# Reproducability
+seed = 1
+np.random.seed(seed)
+tf.random.set_seed(seed)
