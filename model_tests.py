182894641cb7b82be72d4b1e021fe74cf0ca2afb
sumsar01 <skriver@live.dk>
2021-09-29 13:38:55 +0200

    added dropout and data augmentation to models

--- model_tests.py
+++ model_tests.py
@@ -1,37 +1,40 @@
+from PIL.Image import TRANSPOSE
 from tensorflow.python import module
 from plotting import summarize_diagnostics
 from models import basic_model
 from keras.preprocessing.image import ImageDataGenerator
 
-# run test for basic model
-def run_basic_model():
+
+def run_model(model, IMG_SIZE, model_name):
     
-    # initilizing basic model
-    model = basic_model()
+    IMG_SIZE = IMG_SIZE
+    
+    # initilizing model
+    model = model
 
     # create data generator
-    data_generator = ImageDataGenerator(rescale=1.0/255.0)
+    train_data_generator = ImageDataGenerator(rescale=1.0/255.0, width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+    test_data_generator = ImageDataGenerator(rescale=1.0/255.0)
 
     # prepare iterators
-    train_ite = data_generator.flow_from_directory('data_cats_vs_dogs/train/',
+    train_ite = train_data_generator.flow_from_directory('data_cats_vs_dogs/train/',
         class_mode='binary',
         batch_size = 64,
-        target_size=(32, 32)#(200, 200)
+        target_size=(IMG_SIZE, IMG_SIZE)
         )
 
-    validation_ite = data_generator.flow_from_directory('data_cats_vs_dogs/validation/',
+    validation_ite = test_data_generator.flow_from_directory('data_cats_vs_dogs/validation/',
         class_mode='binary',
         batch_size= 64,
-        target_size=(32, 32)#(200, 200)
+        target_size=(IMG_SIZE, IMG_SIZE)
         )
 
     # fit model
-
     history = model.fit(train_ite,
         steps_per_epoch=len(train_ite),
         validation_data=validation_ite,
         validation_steps=len(validation_ite),
-        epochs=10,
+        epochs=20,
         verbose=1
         )
 
@@ -40,7 +43,8 @@ def run_basic_model():
     print('> %.3f' % (acc * 100.0))
 
     #learning curves
-    summarize_diagnostics(history, 'Basic_model')
+    summarize_diagnostics(history, model_name)
 
 if __name__ == '__main__':
-    run_basic_model()
+    IMG_SIZE = 32
+    run_model(basic_model, IMG_SIZE)
--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Dense
 from keras.layers import Flatten
+from keras.layers import Dropout
 from keras.optimizers import SGD
 
 
@@ -20,4 +21,54 @@ def basic_model():
     model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
-basic_model()
+def two_block_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3,3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Conv2D(64, (3,3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(1, activation='sigmoid'))
+
+    #compile model
+    opt = SGD(lr=0.001, momentum=0.9)
+    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+def three_block_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3,3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Conv2D(64, (3,3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Conv2D(128, (3,3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(1, activation='sigmoid'))
+
+    #compile model
+    opt = SGD(lr=0.001, momentum=0.9)
+    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+def VGG3_withDropout_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3,3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(64, (3,3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(128, (3,3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(1, activation='sigmoid'))
+
+    #compile model
+    opt = SGD(lr=0.001, momentum=0.9)
+    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+    return model
